10cfd04dc4f11216cc061826be446da714c2a370
James Diacono <james@diacono.com.au>
2018-10-01 02:21:29 +1000

    🐛 bug: avoid running twice on modern builds

--- src/BundleServiceWorkerPlugin.js
+++ src/BundleServiceWorkerPlugin.js
@@ -3,12 +3,17 @@ const buildSW = require('./build-sw')
 const ID = 'vue-cli:bundle-service-worker-plugin'
 
 module.exports = class GenerateIconsPlugin {
-  constructor ({ buildOptions }) {
+  constructor({ buildOptions }) {
     this.buildOptions = buildOptions
   }
 
   apply(compiler) {
     compiler.hooks.emit.tapPromise(ID, async (compilation) => {
+      if (process.env.VUE_CLI_MODERN_BUILD) {
+        // avoid running twice (already run after the legacy build)
+        return
+      }
+
       await buildSW(this.buildOptions)
     })
   }
